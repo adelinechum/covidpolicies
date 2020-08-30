7f9d18518be0739b3dcdd0afdec5304138919dfb
adelinechum <ac4626@columbia.edu>
2020-08-29 20:08:41 -0400

    filter function

--- fingerprint.js
+++ fingerprint.js
@@ -1,21 +1,87 @@
-var dataTable;
-
+var trendingData
+var timelineData
+var data
+const filteringState = {sortType: 'name', filterType: 'All'};
 
 // main async function, used to wait for completion of asynch tasks
 (async function() {
-  await setUpQuery();
-  dataTable = await getQuery();
+  google.charts.load('current');
+  await setUpQuery('https://docs.google.com/spreadsheets/d/1r2KLSBgiou-osetpUPE3lp2nLh5dWOjuPScGft81AoY/gviz/tq?sheet=RAW_NYT_us-states',
+'select *');
+  var dataTable = await getQuery();
+  var trendingData = createTrendingData(createTable(dataTable));
+  await setUpQuery('https://docs.google.com/spreadsheets/d/1r2KLSBgiou-osetpUPE3lp2nLh5dWOjuPScGft81AoY/gviz/tq?sheet=July31_Timeline',
+'select A, B, C, D, E, F, G, H, I')
+  dataTable = await getQuery()
 
-  processTimelineData(dataTable);
+  timelineData = createtimelineData(createTable(dataTable))
 
+  data = combineData(trendingData, timelineData);
+
+  initGraphs(data)
+
+// loop through each state and render data
+  update(data);
 })();
 
-function setUpQuery() {
+// update data and render
+// TODO: refactor so that data doesn't have to be passed so often
+function update(data) {
+
+var data = filterBy(filteringState.filterType, sortBy(filteringState.sortType,data))
+
+alert('updating')
+
+renderAll(data)
+
+}
+
+function setSort(sortType, data) {
+  filteringState.sortType = sortType
+  update(data)
+}
+
+function setFilter(filterType, data) {
+  filteringState.filterType = filterType
+  update(data)
+}
+
+function sortBy(sortType, data) {
+  switch (sortType) {
+    case 'cases':
+      data = data.sort(function(a,b){
+        return b.currentCases - a.currentCases;
+      })
+      break;
+
+    case 'deaths':
+      data = data.sort(function(a,b){
+        return b.currentDeaths - a.currentDeaths;
+      })
+      break;
+
+    default:
+    data = data.sort((a,b) =>a.state.localeCompare(b.state))
+
+  }
+
+return data;
+
+}
+
+function filterBy(filterType, data) {
+  if(filterType == 'All') {return data}
+  return data.filter(d => {
+    return d.pact == filterType
+  })
+}
+
+// set up google visualization query
+function setUpQuery(sheetLocation, queryStatement) {
    var promise = new Promise(function(resolve, reject) {
-       google.charts.load('current');
        google.charts.setOnLoadCallback(function(){
-           query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1r2KLSBgiou-osetpUPE3lp2nLh5dWOjuPScGft81AoY/gviz/tq?sheet=COMBINED');
-           query.setQuery('select A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T');
+           query = new google.visualization.Query(sheetLocation);
+           query.setQuery(queryStatement);
           resolve('done');
        }
 );
@@ -23,6 +89,7 @@ function setUpQuery() {
    return promise;
 }
 
+// send query and return the response
 function getQuery(){
   var promise = new Promise(function(resolve, reject){
     query.send(function(response){
@@ -34,45 +101,320 @@ function getQuery(){
 
 var sort;
 
-function processTimelineData(response) {
+// transform google response into an array
+function createTable(response){
+  var dataArray =[];
+  var data = response.getDataTable();
+  var columns = data.getNumberOfColumns();
+  var rows = data.getNumberOfRows();
+  var table = [];
+
+  for (var r = 0; r < rows; r++) {
+    var row = [];
+    for (var c = 0; c < columns; c++) {
+      row.push(data.getFormattedValue(r, c));
+    }
+    table.push(row)
+  }
+
+  return table;
+}
+
+// create json from array of data for trending data
+function createTrendingData(table){
+
+    table = table.map(d =>
+      { return {
+        date: new Date(d[0]),
+        state: d[1],
+        fips: +d[2],
+        cases: +d[3],
+        deaths: +d[4],
+        stateDate: d[5],
+        population: +d[6],
+        caseCapita: +d[7],
+        pact: d[8],
+        color: d[9],
+      }}
+    )
+
+return table;
+
+  }
+
+// create json from array of data for time line data
+function createtimelineData(table) {
+
+      table = table.map(d =>
+        { return {
+          policy: d[0],
+          start: new Date(d[1]),
+          end: new Date(d[2]),
+          start2: new Date(d[3]),
+          end2: new Date(d[4]),
+          state: d[5],
+          color: d[6],
+          pact: d[7],
+          notes: d[8],
+        }}
+      )
+
+  return table;
+}
+
+// combine trending Data and timelineData into a single json
+function combineData(trendingData, timelineData){
+
+  var data = []
+// sort timeline data alphabetically so it can be grouped correcly
+  timelineData = timelineData.sort((a,b) =>a.state.localeCompare(b.state))
+
+  for(var i = 0; i< timelineData.length; i = i + 9){
+
+    var casesData = trendingData.filter(d => {
+      return d.state == timelineData[i].state})
+
+    var timeline = timelineData.slice(i, i + 9)
+
+    data.push({
+
+      state: timelineData[i].state,
+      // TODO: remove state, color, pact
+      timeline: timeline,
+      // TODO: remove state, stateDate and pact from casesData
+      casesData: casesData,
+      color: casesData[0].color,
+      currentCases: d3.max(casesData.map(d => d.cases)),
+      currentDeaths: d3.max(casesData.map(d => d.deaths)),
+      firstPolicy: d3.min(timeline.map(d => d.start)),
+      pact: casesData[0].pact
+    })
+  }
+  return data;
+}
+
+
+
+function renderAll(data) {
+  d3.select("#fingerprint").selectAll("*").remove()
+
+// TODO: add the loading bar
+
+  data.forEach((state) => {
+        renderData(state, d3.select("#fingerprint").append("svg"));
+  });
+
+// // TODO: renders out of order
+  // d.forEach((state) => {
+  //     setTimeout(function () {
+  //       renderData(state, d3.select("#fingerprint").append("svg"));
+  //     },0)
+  // });
+  // console.log(data);
+}
+
+
+
+var margin, width, height, maxDate, casesColor, timelineColor, deathColor, yCases, yDeaths
+
+// initalize variables used by all graphs
+
+function initGraphs(data) {
+  // properties of svg size
+
+  margin = ({top: 5, right: 0, bottom: 0, left: 60});
+  width = 420;
+  height = 450;
+  // alternative maxDate based on last date in data
+  // var maxDate = d3.max(timeLineArray.map(d => d.end));
+  maxDate = new Date(2020,08,01);
+
+  // y-axis for timeline
+  yTimeline = d3.scaleBand()
+    .domain(d3.range(data[0].timeline.length))
+    .range([margin.top + height/3, 15 * data[0].timeline.length - margin.bottom + height/3])
+
+  // color scale for cases
+  casesColor = d3.scaleOrdinal()
+    .domain(['Republican','Swing','Democrat'])
+    .range(['#F03F4E','#A47AF0','#6292F0'])
+
+  // color scale for timeline
+  timelineColor = d3.scaleOrdinal()
+    .domain(['Republican','Swing','Democrat'])
+    .range(['#D60012','#4B00D6','#0047D6'])
+
+  // color scale for death graph
+  deathColor = d3.scaleOrdinal()
+    .domain(['Republican','Swing','Democrat'])
+    .range(['#570007','#1E0057','#001D57'])
+
+  // y-axis for cases
+  yCases = d3.scaleLinear()
+    .domain([0, .03])
+    .range([height/3,0 + margin.top]);
+
+  // y-axis for death graph
+  yDeaths = d3.scaleLinear()
+  .domain([0, 32500])
+  .range([height * 2/3,height])
+
+}
+
+// TODO: change so that we don't have to remove svg elements every time.
+// should be able to just update with d3
+function renderData(state, svg){
+
+// get min and max date
+var minDate = state.firstPolicy
+
+// x-axis for all graphs
+x = d3.scaleLinear()
+  .domain([minDate, maxDate])
+  .range([margin.left, width - margin.right])
+  .clamp(true);
+
+  {
+    // create timelines
+    svg
+      .attr("width", width)
+      .attr("height", height+ margin.bottom + margin.top)
+      // .attr("height", (y.range()[1]+ margin.bottom + margin.top) * 3)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "10")
+      .attr("text-anchor", "end");
+
+    svg.append("text").text(state.state + " Cases: " + d3.format(",")(state.currentCases)
+    + ", Deaths: " + d3.format(",")(state.currentDeaths))
+    .attr("transform", `translate(${margin.left + 250}, 10)`);
+
+    const bar = svg.selectAll("g")
+    .data(state.timeline)
+    .join("g")
+      .attr("transform", (d, i) => `translate(0,${yTimeline(i)})`);
+
+    bar.append("rect")
+      // .attr("fill", "steelblue")
+      .attr("fill", d => timelineColor(d.color))
+      .attr("width", d => x(d.end - d.start + minDate.getTime()))
+
+      .attr("x", d => x(d.start))
+      .attr("height", yTimeline.bandwidth() - 1);
+
+    bar.append("text")
+      .attr("fill", "white")
+      .attr("x", d => x(d.end) - 3)
+      .attr("y", yTimeline.bandwidth() / 2)
+      .attr("dy", "0.35em")
+      .text(d => d.policy);
+
+// x axis
+    svg.append("g")
+      .attr("transform", `translate(0,${yTimeline.range()[1]})`)
+      .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%m/%Y")));
+
+
+// create trending graphs
+      svg.append("g")
+      .attr("transform", `translate(${margin.left},${0})`)
+      .call(d3.axisLeft(yCases).ticks(5));
+
+      const vertical_bars = svg
+      .selectAll(".vb")
+      .data(state.casesData)
+      .enter().append("g").attr("class","vb");
+      // .attr("transform", d => `translate(${x(d.date)},${height/3})`);
+
+      vertical_bars.append("rect")
+        .attr("width", 1)
+        .attr("fill", d => casesColor(d.color))
+        // .attr("fill", "steelblue")
+        .attr("x", d => x(d.date))
+        .attr("height", d => height/3 - yCases(d.caseCapita))
+        .attr("y", d => yCases(d.caseCapita))
+        ;
+
+        // y axis
+        svg.append("g")
+        .attr("transform", `translate(${margin.left},${0})`)
+        .call(d3.axisLeft(yCases).ticks(5));
+
+        svg.append("g")
+        .attr("transform", `translate(${margin.left},${0})`)
+        .call(d3.axisLeft(yDeaths));
+
+        const vertical_bars_2 = svg
+        .selectAll(".vb2")
+        .data(state.casesData)
+        .enter().append("g").attr("class","vb2");
+
+        vertical_bars_2.append("rect")
+          .attr("width", 1)
+          .attr("fill", d => deathColor(d.color))
+          // .attr("fill", "steelblue")
+          .attr("x", d => x(d.date))
+          .attr("height", d => yDeaths(d.deaths) - height * 2/3)
+          .attr("y", height * 2/3);
+
+      }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function processtimelineData(response) {
 
   // console.log(response)
   var timeLineArray = [];
   var casesArray = []
-  var timeLineData = response.getDataTable();
+  var timelineData = response.getDataTable();
   dataTable = response.getDataTable();
 
-  var casesData = timeLineData.clone()
+  var casesData = timelineData.clone()
 
-  timeLineData.removeColumns(0,11)
+  timelineData.removeColumns(0,11)
 
   casesData.removeColumns(10,10)
 
-  var columns = timeLineData.getNumberOfColumns();
-  var rows = timeLineData.getNumberOfRows();
+  var columns = timelineData.getNumberOfColumns();
+  var rows = timelineData.getNumberOfRows();
 
   var emptyRow;
 
   // find empty row
   for (var r = 0; r < rows; r++) {
-    if (timeLineData.getFormattedValue(r,0) == ""){
+    if (timelineData.getFormattedValue(r,0) == ""){
       emptyRow = r;
       break;
     }
   }
 
 // truncate data at empty row
-  timeLineData.removeRows(emptyRow, rows - emptyRow);
+  timelineData.removeRows(emptyRow, rows - emptyRow);
 
-  var columns = timeLineData.getNumberOfColumns();
-  var rows = timeLineData.getNumberOfRows();
+  var columns = timelineData.getNumberOfColumns();
+  var rows = timelineData.getNumberOfRows();
   for (var r = 0; r < rows; r++) {
     var row = [];
     for (var c = 0; c < columns; c++) {
-      row.push(timeLineData.getFormattedValue(r, c));
+      row.push(timelineData.getFormattedValue(r, c));
     }
 
-  var columns = timeLineData.getNumberOfColumns();
+  var columns = timelineData.getNumberOfColumns();
 
   timeLineArray.push({
       policy: row[0],
@@ -95,7 +437,7 @@ var maxDate = new Date(2020,08,01);
 //
 
 
-timeLineData = [];
+timelineData = [];
 
 var columns = casesData.getNumberOfColumns();
 var rows = casesData.getNumberOfRows();
@@ -127,7 +469,7 @@ for(var i = 0; i< timeLineArray.length; i = i + 9){
 
   var data = timeLineArray.slice(i, i + 9)
 
-  timeLineData.push({
+  timelineData.push({
 
     state: timeLineArray[i].state,
 
@@ -147,34 +489,25 @@ for(var i = 0; i< timeLineArray.length; i = i + 9){
   })
 }
 
-// console.log(timeLineData);
+// console.log(timelineData);
 
 if(sort == 'cases'){
-  timeLineData.sort(function(a,b){
+  timelineData.sort(function(a,b){
     return b.currentCases - a.currentCases;
   })
 }
 
 else if(sort == 'deaths'){
-  timeLineData.sort(function(a,b){
+  timelineData.sort(function(a,b){
     return b.currentDeaths - a.currentDeaths;
   })
 }
-console.log(timeLineData);
 
-timeLineData.forEach((stateData) => {
-    renderData(stateData, d3.select("#fingerprint").append("svg"));
+timelineData.forEach((stateData) => {
+    renderData(stateData, d3.select("body").append("svg"));
 });
 
-
-  // renderData(array);
-
-
   function renderData(data, svg){
-
-    // customize min and max dates
-  // minDate = d3.min(data.data.map(d => d.start));
-  // maxDate = d3.max(data.data.map(d => d.end));
 
   var margin = ({top: 5, right: 0, bottom: 0, left: 60});
 
@@ -204,11 +537,11 @@ timeLineData.forEach((stateData) => {
     .domain(['Republican','Swing','Democrat'])
     .range(['#570007','#1E0057','#001D57'])
 
-  y2 = d3.scaleLinear()
+  yCases = d3.scaleLinear()
     .domain([0, .03])
     .range([height/3,0 + margin.top]);
 
-  y3 = d3.scaleLinear()
+  yDeaths = d3.scaleLinear()
   .domain([0, 32500])
   .range([height * 2/3,height])
 
@@ -257,7 +590,7 @@ timeLineData.forEach((stateData) => {
         // y axis
         svg.append("g")
         .attr("transform", `translate(${margin.left},${0})`)
-        .call(d3.axisLeft(y2).ticks(5));
+        .call(d3.axisLeft(yCases).ticks(5));
 
         const vertical_bars = svg
         .selectAll(".vb")
@@ -270,18 +603,18 @@ timeLineData.forEach((stateData) => {
           .attr("fill", d => casesColor(d.color))
           // .attr("fill", "steelblue")
           .attr("x", d => x(d.date))
-          .attr("height", d => height/3 - y2(d.caseCapita))
-          .attr("y", d => y2(d.caseCapita))
+          .attr("height", d => height/3 - yCases(d.caseCapita))
+          .attr("y", d => yCases(d.caseCapita))
           ;
 
           // y axis
           svg.append("g")
           .attr("transform", `translate(${margin.left},${0})`)
-          .call(d3.axisLeft(y2).ticks(5));
+          .call(d3.axisLeft(yCases).ticks(5));
 
           svg.append("g")
           .attr("transform", `translate(${margin.left},${0})`)
-          .call(d3.axisLeft(y3));
+          .call(d3.axisLeft(yDeaths));
 
           const vertical_bars_2 = svg
           .selectAll(".vb2")
@@ -293,9 +626,8 @@ timeLineData.forEach((stateData) => {
             .attr("fill", d => deathColor(d.color))
             // .attr("fill", "steelblue")
             .attr("x", d => x(d.date))
-            .attr("height", d => y3(d.deaths) - height * 2/3)
-            .attr("y", height * 2/3)
-            ;
+            .attr("height", d => yDeaths(d.deaths) - height * 2/3)
+            .attr("y", height * 2/3);
 
         }
 
--- index.js
+++ index.js
@@ -3,27 +3,31 @@
 window.onscroll = function() {myFunction()};
 
 function changeElements(background, title, footer, names=""){
-  document.getElementById("header_bar").style.backgroundImage = background;
+  document.getElementById("imagecontainer").style.backgroundImage = background;
   document.getElementById("title").innerHTML = title;
   // document.getElementById("footer").innerHTML = footer;
 }
 
 function myFunction() {
   var margin = 200;
-  if (document.body.scrollTop > margin * 18 || document.documentElement.scrollTop > margin * 18) {
+  if (document.body.scrollTop > margin * 30 || document.documentElement.scrollTop > margin * 30) {
     changeElements("url('')",
     "Flatten the Curve: Fingerprints",
     "")
-  } else if (document.body.scrollTop > margin * 15|| document.documentElement.scrollTop > margin * 11) {
+  } else if (document.body.scrollTop > margin * 25|| document.documentElement.scrollTop > margin * 25) {
     changeElements("url('images/Explainer.png')",
     "Flatten the Curve: Fingerprint Explainer",
-    "The Coastal GasLink pipeline plans to cut through several indigenious territories.")
-  } else if (document.body.scrollTop > margin * 7 || document.documentElement.scrollTop > margin * 7) {
+    "")
+  } else if (document.body.scrollTop > margin * 10 || document.documentElement.scrollTop > margin * 10) {
     changeElements("url('')",
     "Flatten the Curve: Graphs",
     "")
+  } else if (document.body.scrollTop > margin * 5 || document.documentElement.scrollTop > margin * 5) {
+    changeElements("url('images/Orders_2.gif')",
+    "Flatten the Curve: Introduction to Policies",
+    "")
   } else {
-    changeElements("url('images/Orders.gif')",
+    changeElements("url('images/Orders_1.gif')",
     "Flatten the Curve: Introduction to Policies",
     "Mess of Orders"
   )
